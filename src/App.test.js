73b54b795ee62afb47ba7543fa07a7bf258d5daa
=Riky <=ra_mo116@hotmail.com>
2025-01-08 15:58:03 -0800

    changed test import

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
