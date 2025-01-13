c20682fdfd7169515263a95a37b657ba9258a916
=Riky <=ra_mo116@hotmail.com>
2025-01-13 15:54:25 -0800

    label and price were displayed on page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,56 +6,71 @@ import Typography from '@mui/material/Typography';
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 import Stack from '@mui/material/Stack';
 import '../css/Home.css'
+
+import { useTreeViewApiRef } from '@mui/x-tree-view/hooks';
+
 const MUI_X_PRODUCTS = [
   {
     id: 'Bread',
     label: 'Bread',
     children: [
-      { id: 'Chocolate-Bread', label: 'Chocolate' },
-      { id: 'Vanilla-Bread', label: 'Vanilla' },
+      { id: 'Chocolate-Bread', label: 'Chocolate' , price: '45'},
+      { id: 'Vanilla-Bread', label: 'Vanilla' , price: '25' },
     ],
   },
   {
     id: 'Filling',
     label: 'Filling',
     children: [
-      { id: 'Banana-Filling', label: 'Banana' },
-      { id: 'Cream-Filling', label: 'Cream' },
+      { id: 'Banana-Filling', label: 'Banana', price: '55' },
+      { id: 'Cream-Filling', label: 'Cream', price: '95' },
     ],
   },
   {
     id: 'Cover',
     label: 'Cover',
-    children: [{ id: 'Chantilly-Cream-Cover', label: 'Chantilly Cream' },
-               { id: 'Butter-Cream-Cover', label: 'Butter Cream' }
+    children: [{ id: 'Chantilly-Cream-Cover', label: 'Chantilly Cream', price: '75' },
+               { id: 'Butter-Cream-Cover', label: 'Butter Cream', price: '13' }
     ],
   },
   {
     id: 'Decoration',
     label: 'Decoration',
-    children: [{ id: 'Birthday-Decoration', label: 'Birthday' },
-               { id: 'Weedings-Decoration', label: 'Weedings' }
+    children: [{ id: 'Birthday-Decoration', label: 'Birthday', price: '78' },
+               { id: 'Weedings-Decoration', label: 'Weedings', price: '64' }
     ],
   },
   {
     id: 'Size',
     label: 'Size',
-    children: [{ id: 'Small-Size', label: 'Small' },
-               { id: 'Medium-Size', label: 'Medium' },
-               { id: 'Large-Size', label: 'Large' }
+    children: [{ id: 'Small-Size', label: 'Small', price: '99' },
+               { id: 'Medium-Size', label: 'Medium', price: '74' },
+               { id: 'Large-Size', label: 'Large', price: '68' }
     ],
   },
 ];
 
 
 const Home = () => {
+
+  const apiRef = useTreeViewApiRef();
+
   const [lastSelectedItem, setLastSelectedItem] = React.useState(null);
 
+  const [selectedItem, setSelectedItem] = React.useState(null);
+
+  const handleSelectedItemsChange = (event, itemId) => {
+    if (itemId == null) {
+      setSelectedItem(null);
+    } else {
+      setSelectedItem(apiRef.current.getItem(itemId));
+    }
+  };
 
   const handleItemSelectionToggle = (event, itemId, isSelected) => {
     if (isSelected) {
       setLastSelectedItem(itemId);
-     
+  
     }
   };
 
@@ -70,22 +85,33 @@ const Home = () => {
       <Typography>
         <b>Create Your Cake:</b>
       </Typography>
-      <Box sx={{ minHeight: 352, minWidth: 300 }}>
-        <RichTreeView
-          items={MUI_X_PRODUCTS}
-          onItemSelectionToggle={handleItemSelectionToggle}
-        />
+        <Box sx={{ minHeight: 352, minWidth: 300 }}>
+          <RichTreeView
+            items={MUI_X_PRODUCTS}
+            
+            onItemSelectionToggle={handleItemSelectionToggle}
+
+            apiRef={apiRef}
+            selectedItems={selectedItem?.id ?? null}
+            onSelectedItemsChange={handleSelectedItemsChange}
+          />
+        </Box>
+      </Stack>
       </Box>
-    </Stack>
-    </Box>
           
-        </Grid>
+      </Grid>
         <Grid size={{ xs: 8, md: 10 }}>
           <h1>Home</h1>
           <Typography>
             {lastSelectedItem == null
               ? 'No item selection recorded'
-              : `Last selected item: ${lastSelectedItem}`}
+              : `Last selected item id: ${lastSelectedItem}`}
+          </Typography>
+ 
+          <Typography sx={{ minWidth: 300 }}>
+            Selected item : {selectedItem == null ? 'none' : ( selectedItem.label + " " ) } 
+                             
+                            { selectedItem == null ? ' none' : ( selectedItem.price )  }
           </Typography>
         </Grid>
       </Grid>
