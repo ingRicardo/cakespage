d73615972a894afc57ee4e517000b9692686b13c
=Riky <=ra_mo116@hotmail.com>
2025-01-15 11:37:53 -0800

    add func to get total price

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,38 +15,38 @@ const MUI_X_PRODUCTS = [
     id: 'Bread',
     label: 'Bread',
     children: [
-      { id: 'Chocolate-Bread', label: 'Chocolate' , price: '45'},
-      { id: 'Vanilla-Bread', label: 'Vanilla' , price: '25' },
+      { id: 'Chocolate-Bread', label: 'Chocolate' , price: '45.00'},
+      { id: 'Vanilla-Bread', label: 'Vanilla' , price: '25.00' },
     ],
   },
   {
     id: 'Filling',
     label: 'Filling',
     children: [
-      { id: 'Banana-Filling', label: 'Banana', price: '55' },
-      { id: 'Cream-Filling', label: 'Cream', price: '95' },
+      { id: 'Banana-Filling', label: 'Banana', price: '55.00' },
+      { id: 'Cream-Filling', label: 'Cream', price: '95.00' },
     ],
   },
   {
     id: 'Cover',
     label: 'Cover',
-    children: [{ id: 'Chantilly-Cream-Cover', label: 'Chantilly Cream', price: '75' },
-               { id: 'Butter-Cream-Cover', label: 'Butter Cream', price: '13' }
+    children: [{ id: 'Chantilly-Cream-Cover', label: 'Chantilly Cream', price: '75.00' },
+               { id: 'Butter-Cream-Cover', label: 'Butter Cream', price: '13.00' }
     ],
   },
   {
     id: 'Decoration',
     label: 'Decoration',
-    children: [{ id: 'Birthday-Decoration', label: 'Birthday', price: '78' },
-               { id: 'Weedings-Decoration', label: 'Weedings', price: '64' }
+    children: [{ id: 'Birthday-Decoration', label: 'Birthday', price: '78.00' },
+               { id: 'Weedings-Decoration', label: 'Weedings', price: '64.00' }
     ],
   },
   {
     id: 'Size',
     label: 'Size',
-    children: [{ id: 'Small-Size', label: 'Small', price: '99' },
-               { id: 'Medium-Size', label: 'Medium', price: '74' },
-               { id: 'Large-Size', label: 'Large', price: '68' }
+    children: [{ id: 'Small-Size', label: 'Small', price: '99.00' },
+               { id: 'Medium-Size', label: 'Medium', price: '74.00' },
+               { id: 'Large-Size', label: 'Large', price: '68.00' }
     ],
   },
 ];
@@ -54,6 +54,7 @@ const MUI_X_PRODUCTS = [
 
 const Home = () => {
   const [list, setList] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
 
   const apiRef = useTreeViewApiRef();
 
@@ -75,11 +76,14 @@ const Home = () => {
          const uniqPrices = [...new Set(newList.map(item => (  item.price  )))]
 
          var listtmp =[];
+         var tot =0.0;
          for (let i = 0; i < uniqIds.length; i++) {
            listtmp = listtmp.concat({ id: uniqIds[i], label:  uniqLabels[i] , price: uniqPrices[i]} );
+           tot += parseInt(uniqPrices[i]);
          }
          setList(listtmp);
-
+         setTotal(tot);
+         console.log(total);
       }
   
     }
@@ -89,7 +93,7 @@ const Home = () => {
   function handleRemove() {
  
     setList([]);
- 
+    setTotal(0);
   }
 
 
@@ -134,7 +138,12 @@ const Home = () => {
             <li key={item.id}>{item.id} ,{item.label} , $ {item.price}</li>
            ))}
           </ul>
-           
+          <div className='containerTitle'>
+            <Typography>
+              <b>TOTAL:</b> <span>$ {total}</span>
+            </Typography>
+          </div>
+        
         </Grid>
       </Grid>
     </Box>
