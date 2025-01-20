88c305e8c70e71958f889a70ab99e8571d90e3bf
=Riky <=ra_mo116@hotmail.com>
2025-01-20 11:58:02 -0800

    add comment in posts

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -10,25 +10,61 @@ import '../css/Blog.css'
 const Blogs = () => {
 
   const [inputValue, setInputValue] = React.useState('');
+  const [idValue, setIdValue] = React.useState('');
+
   var [articles, setArticles] = React.useState(
     [
-      { id: 1, content: "How to create a fondant cake"},
-      { id: 2, content: "I like to bake sweet cakes"},
-      { id: 3, content: "My favorite dessert is a vanilla cake with banana"},
-      { id: 4, content: "I love cakes"},
-      { id: 5, content: "This is about selling cakes"}
+      { id: 1, content: "How to create a fondant cake"  , txtcontent: "great"},
+      { id: 2, content: "I like to bake sweet cakes", txtcontent: "nice"},
+      { id: 3, content: "My favorite dessert is a vanilla cake with banana", txtcontent: "cool"},
+      { id: 4, content: "I love cakes", txtcontent: "pretty well"},
+      { id: 5, content: "This is about selling cakes", txtcontent: "just do it"}
     ]);
 
     const handleInputChange = (event) => {
       setInputValue(event.target.value);
 
     };
+ 
+
 
     const shoot = () => {
       if(inputValue!==''){
         const newList = articles.concat({ id: articles.length+1, content: inputValue  } );
         setArticles(newList);
       }
+    }
+
+ 
+
+    const [jsonValue, setJsonValue] = React.useState('');
+
+    function handleSubmit(e) {
+      // Prevent the browser from reloading the page
+      e.preventDefault();
+  
+      // Read the form data
+      const form = e.target;
+      const formData = new FormData(form);
+  
+      // You can pass formData as a fetch body directly:
+     // fetch('/some-api', { method: form.method, body: formData });
+  
+      // Or you can work with it as a plain object:
+      const formJson = Object.fromEntries(formData.entries());
+ 
+    
+      setJsonValue(formJson);
+
+ 
+
+      const found = articles.filter(articles  =>{
+        if (articles.id == formJson['postId'])
+          setIdValue(articles.id); 
+      
+    });
+
+ 
     }
 
 
@@ -52,17 +88,38 @@ const Blogs = () => {
           <Button variant="contained" onClick={shoot} size="large">
           Add
         </Button>
+ 
         <React.Fragment>
           { 
              <div className='maincl' >  
-            
+             
             {
+              
+              
               articles.map(r =><>
-              <div>  {r.id + " " + r.content} </div>
-              <textarea name="postContent" rows={4} cols={40} />
-              <div>  
-                         <Button variant="contained"  size="small"> Post</Button>
-              </div>
+                <div>  {r.id + " " + r.content} </div>
+                  <p>{r.txtcontent}</p>
+                   {idValue === r.id ? (<p> title : {jsonValue.postTitle} comment: {jsonValue.postContent}</p>):""}
+                  <form method="post" onSubmit={handleSubmit}  >
+                    <label>
+                      Post title: <input name="postTitle" defaultValue="Eating"  />
+                     
+                      <input name="postId" defaultValue= {r.id}  type="hidden" />
+                    </label>
+                    <label>
+                      Edit your post:
+                      <textarea
+                        name="postContent"
+                        defaultValue="I really enjoyed eating cakes yesterday!"
+                        rows={4}
+                        cols={40}
+                        
+                      />
+                    </label>
+                    <hr />
+                    <Button type="reset">Reset edits</Button>
+                    <Button type="submit">Save post</Button>
+                  </form>
                </>)
             }
             
