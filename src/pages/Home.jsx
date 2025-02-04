6d6588e5649bac46ff971f9ea0d91938430a9f79
=Riky <=ra_mo116@hotmail.com>
2025-02-04 11:58:04 -0800

    added carrousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,8 +60,8 @@ const MUI_X_PRODUCTS = [
     id: 'Bread',
     label: 'Bread',
     children: [
-      { id: 'Chocolate-Bread', label: 'Chocolate' , price: '45.00'},
-      { id: 'Vanilla-Bread', label: 'Vanilla' , price: '25.00' },
+      { id: 'Chocolate-Bread', label: 'Chocolate', price: '45.00' },
+      { id: 'Vanilla-Bread', label: 'Vanilla', price: '25.00' },
     ],
   },
   {
@@ -76,22 +76,22 @@ const MUI_X_PRODUCTS = [
     id: 'Cover',
     label: 'Cover',
     children: [{ id: 'Chantilly-Cream-Cover', label: 'Chantilly Cream', price: '75.00' },
-               { id: 'Butter-Cream-Cover', label: 'Butter Cream', price: '13.00' }
+    { id: 'Butter-Cream-Cover', label: 'Butter Cream', price: '13.00' }
     ],
   },
   {
     id: 'Decoration',
     label: 'Decoration',
     children: [{ id: 'Birthday-Decoration', label: 'Birthday', price: '78.00' },
-               { id: 'Weedings-Decoration', label: 'Weedings', price: '64.00' }
+    { id: 'Weedings-Decoration', label: 'Weedings', price: '64.00' }
     ],
   },
   {
     id: 'Size',
     label: 'Size',
     children: [{ id: 'Small-Size', label: 'Small', price: '99.00' },
-               { id: 'Medium-Size', label: 'Medium', price: '74.00' },
-               { id: 'Large-Size', label: 'Large', price: '68.00' }
+    { id: 'Medium-Size', label: 'Medium', price: '74.00' },
+    { id: 'Large-Size', label: 'Large', price: '68.00' }
     ],
   },
 ];
@@ -102,39 +102,39 @@ const Home = () => {
   const [total, setTotal] = React.useState(0);
   const apiRef = useTreeViewApiRef();
   const [selectedItem, setSelectedItem] = React.useState(null);
-  const [emailAddress, setEmail] = React.useState( );
+  const [emailAddress, setEmail] = React.useState();
 
   const handleSelectedItemsChange = (event, itemId) => {
     if (itemId == null) {
       setSelectedItem(null);
     } else {
       setSelectedItem(apiRef.current.getItem(itemId));
-   
-      if(apiRef.current.getItem(itemId).price){
-        const newList = list.concat({ id: itemId, label:  apiRef.current.getItem(itemId).label , price: apiRef.current.getItem(itemId).price} );
-          
+
+      if (apiRef.current.getItem(itemId).price) {
+        const newList = list.concat({ id: itemId, label: apiRef.current.getItem(itemId).label, price: apiRef.current.getItem(itemId).price });
+
         setList(newList);
 
-         const uniqIds = [...new Set(newList.map(item => (  item.id  )))]
-         const uniqLabels = [...new Set(newList.map(item => (  item.label  )))]
-         const uniqPrices = [...new Set(newList.map(item => (  item.price  )))]
+        const uniqIds = [...new Set(newList.map(item => (item.id)))]
+        const uniqLabels = [...new Set(newList.map(item => (item.label)))]
+        const uniqPrices = [...new Set(newList.map(item => (item.price)))]
 
-         var listtmp =[];
-         var tot =0.0;
-         for (let i = 0; i < uniqIds.length; i++) {
-           listtmp = listtmp.concat({ id: uniqIds[i], label:  uniqLabels[i] , price: uniqPrices[i]} );
-           tot += parseInt(uniqPrices[i]);
-         }
-         setList(listtmp);
-         setTotal(tot);
+        var listtmp = [];
+        var tot = 0.0;
+        for (let i = 0; i < uniqIds.length; i++) {
+          listtmp = listtmp.concat({ id: uniqIds[i], label: uniqLabels[i], price: uniqPrices[i] });
+          tot += parseInt(uniqPrices[i]);
+        }
+        setList(listtmp);
+        setTotal(tot);
       }
-  
+
     }
   };
 
- 
+
   function handleRemove() {
- 
+
     setList([]);
     setTotal(0);
     setEmail();
@@ -155,76 +155,158 @@ const Home = () => {
 
     setList(newList);
 
-    var tot =0.0;
+    var tot = 0.0;
     for (let i = 0; i < newList.length; i++) {
-      
+
       tot += parseInt(newList[i]['price']);
     }
-   
+
     setTotal(tot);
 
   }
 
-    return (
+  return (
     <>
-    <Box sx={{ flexGrow: 1 }}  >
-      <Grid container spacing={2} >
-      <Grid size={{ xs: 6, md: 6 }}>
-          <h1>Home</h1>
-          <Box  sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1 }}  >
+        <Grid container spacing={2} >
+          <Grid size={{ xs: 6, md: 6 }}>
+            <h1>Home</h1>
+            <Box sx={{ flexGrow: 1 }}>
 
-          {list && list.length >0
-            ? (<><div className='containerTitle'>
-              <Typography>
-                <b className='textstyle'>Your Cake:</b>
-                <Button type="button" onClick={handleRemove}>
-                  X
-                </Button>
-              </Typography>
-              </div>
-
-              <List sx={{ width: '100%',   bgcolor: 'background.paper' }}>
-                {emailAddress ? (<>
+              {list && list.length > 0
+                ? (<><div className='containerTitle'>
                   <Typography>
-                    <b>Your E-MAIL:</b> <span> {emailAddress}</span>
-                  </Typography></>) : ''}
-        
-                {list.map((item) => (
-                      <>
-                      <ListItem alignItems="flex-start" key={item.id}>
-          
-                        <ListItemText 
-                          primary={item.id}
-                          secondary={
-                            <React.Fragment>
-                              <Typography
-                                component="span"
-                                variant="body2"
-                                sx={{ color: 'text.primary', display: 'inline' }}
-                              >
-                               {item.label}<span>_</span>
-                              </Typography>
-                                 $ {item.price} pesos
-                            </React.Fragment>
-                          }
-                        />
-                        <Button type="button" onClick={() => handleRemoveItem(item.id)}>
-                                 X
-                        </Button>
-                      </ListItem>
-                      <Divider variant="inset" component="li" />
-                      </>
-                ))}
-
-              </List>
-              <div className='containerTotal'>
-                  <Typography>
-                    <b>TOTAL:</b> <span>$ {total}</span>  <span>pesos</span>           
+                    <b className='textstyle'>Your Cake:</b>
+                    <Button type="button" onClick={handleRemove}>
+                      X
+                    </Button>
                   </Typography>
+                </div>
 
-              </div>
-              </> )
-            : (<> 
+                  <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                    {emailAddress ? (<>
+                      <Typography>
+                        <b>Your E-MAIL:</b> <span> {emailAddress}</span>
+                      </Typography></>) : ''}
+
+                    {list.map((item) => (
+                      <>
+                        <ListItem alignItems="flex-start" key={item.id}>
+
+                          <ListItemText
+                            primary={item.id}
+                            secondary={
+                              <React.Fragment>
+                                <Typography
+                                  component="span"
+                                  variant="body2"
+                                  sx={{ color: 'text.primary', display: 'inline' }}
+                                >
+                                  {item.label}<span>_</span>
+                                </Typography>
+                                $ {item.price} pesos
+                              </React.Fragment>
+                            }
+                          />
+                          <Button type="button" onClick={() => handleRemoveItem(item.id)}>
+                            X
+                          </Button>
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                      </>
+                    ))}
+
+                  </List>
+                  <div className='containerTotal'>
+                    <Typography>
+                      <b>TOTAL:</b> <span>$ {total}</span>  <span>pesos</span>
+                    </Typography>
+                  </div>
+
+                </>)
+                : (<>
+                  <img
+                    className="d-block w-100"
+                    src="https://circle-cakes-app.web.app/assets/img/birthdaycake2.jpg"
+                    alt="wedding One"
+                  />
+                </>)
+              }
+
+            </Box>
+          </Grid>
+          <Grid size={{ xs: 6, md: 6 }}>
+
+            <Box sx={{ flexGrow: 1 }}>
+              <Stack spacing={2}>
+                <Typography>
+                  <b className='textstyle'>Ingredients:</b>
+                </Typography>
+                <Box sx={{ flexGrow: 1 }}>
+                  <RichTreeView
+                    items={MUI_X_PRODUCTS}
+                    apiRef={apiRef}
+                    selectedItems={selectedItem?.id ?? null}
+                    onSelectedItemsChange={handleSelectedItemsChange}
+                    defaultExpandedItems={['grid']}
+                    slots={{ item: CustomTreeItem }}
+                  />
+                </Box>
+                <React.Fragment>
+                  {list && list.length > 0
+                    ?
+                    <Button variant="outlined" onClick={handleClickOpen}>
+                      Add E-mail
+                    </Button> : ""}
+                  <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                      component: 'form',
+                      onSubmit: (event) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        const email = formJson.email;
+
+                        setEmail(email);
+                        handleClose();
+                      },
+                    }}
+                  >
+                    <DialogTitle>E-mail</DialogTitle>
+                    <DialogContent>
+                      <DialogContentText>
+                        To create your cake , please enter your email address here. We
+                        will send updates occasionally.
+                      </DialogContentText>
+                      <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="email"
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                      />
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={handleClose}>Cancel</Button>
+                      <Button type="submit">Subscribe</Button>
+                    </DialogActions>
+                  </Dialog>
+                </React.Fragment>
+
+              </Stack>
+            </Box>
+
+          </Grid>
+          <Grid size={{ xs: 12, md: 12 }}>
+
+            <Box sx={{ flexGrow: 1 }}>
+
               <div style={{ display: 'block', width: '100%', padding: 30 }}>
                 <h4>Delicious Cakes</h4>
                 <Carousel>
@@ -263,86 +345,14 @@ const Home = () => {
                   </Carousel.Item>
                 </Carousel>
               </div>
-              
-              </>)
-          }
 
-          </Box>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid size={{ xs: 6, md: 6 }}>
-           
-          <Box  sx={{ flexGrow: 1 }}>
-            <Stack spacing={2}>
-            <Typography>
-              <b className='textstyle'>Ingredients:</b>
-            </Typography>
-              <Box sx={{ flexGrow: 1 }}>
-                <RichTreeView
-                  items={MUI_X_PRODUCTS}
-                  apiRef={apiRef}
-                  selectedItems={selectedItem?.id ?? null}
-                  onSelectedItemsChange={handleSelectedItemsChange}
-                  defaultExpandedItems={['grid']}
-                  slots={{ item: CustomTreeItem }}
-                />
-              </Box>
-              <React.Fragment>
-              {list && list.length >0
-            ?
-              <Button variant="outlined" onClick={handleClickOpen}>
-                Add E-mail
-              </Button> :""}
-              <Dialog
-                open={open}
-                onClose={handleClose}
-                PaperProps={{
-                  component: 'form',
-                  onSubmit: (event) => {
-                    event.preventDefault();
-                    const formData = new FormData(event.currentTarget);
-                    const formJson = Object.fromEntries(formData.entries());
-                    const email = formJson.email;
-                    
-                    setEmail(email);
-                    handleClose();
-                  },
-                }}
-              >
-                <DialogTitle>E-mail</DialogTitle>
-                <DialogContent>
-                  <DialogContentText>
-                    To create your cake , please enter your email address here. We
-                    will send updates occasionally.
-                  </DialogContentText>
-                  <TextField
-                    autoFocus
-                    required
-                    margin="dense"
-                    id="name"
-                    name="email"
-                    label="Email Address"
-                    type="email"
-                    fullWidth
-                    variant="standard"
-                  />
-                </DialogContent>
-                <DialogActions>
-                  <Button onClick={handleClose}>Cancel</Button>
-                  <Button type="submit">Subscribe</Button>
-                </DialogActions>
-              </Dialog>
-            </React.Fragment>
-
-            </Stack>
-          </Box>
-          
-      </Grid>
-
-      </Grid>
-    </Box>
+      </Box>
     </>
-    )
-       
-  };
-  
-  export default Home;
+  )
+
+};
+
+export default Home;
