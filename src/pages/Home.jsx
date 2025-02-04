13278467dec78436b81a65ac5b11e94d1c2ccab7
=Riky <=ra_mo116@hotmail.com>
2025-02-04 11:20:22 -0800

    removed unused libraries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,9 +20,6 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import CakeIcon from '@mui/icons-material/Cake';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.css';
 
